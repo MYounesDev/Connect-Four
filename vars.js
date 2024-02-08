520f4ae2402037d3a9591821b1339ee12f34a02a
MY-EG <158834031+MY-EG@users.noreply.github.com>
2024-02-08 23:31:28 +0300

    Add files via upload

---
+++ vars.js
@@ -0,0 +1,1 @@
+var Game={};Game.config={startingPlayer:"black",takenMsg:"This position is already taken. Please make another choice.",drawMsg:"This game is a draw.",winMsg:"The winner is: ",countToWin:4,boardLength:6,boardHeight:5,};Game.board=[[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0]];Game.currentPlayer=Game.config.startingPlayer;
