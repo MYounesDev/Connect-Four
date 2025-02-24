d9790538a73900bcf2469d63465e9bbb83abdfb7
MYunus Aboushewil <mohammadyounes686@gmail.com>
2025-02-24 21:38:58 +0300

    formated

--- vars.js
+++ vars.js
@@ -1,1 +1,19 @@
-var Game={};Game.config={startingPlayer:"black",takenMsg:"This position is already taken. Please make another choice.",drawMsg:"This game is a draw.",winMsg:"The winner is: ",countToWin:4,boardLength:6,boardHeight:5,};Game.board=[[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0]];Game.currentPlayer=Game.config.startingPlayer;
+var Game = {};
+Game.config = {
+  startingPlayer: "black",
+  takenMsg: "This position is already taken. Please make another choice.",
+  drawMsg: "This game is a draw.",
+  winMsg: "The winner is: ",
+  countToWin: 4,
+  boardLength: 6,
+  boardHeight: 5,
+};
+Game.board = [
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+];
+Game.currentPlayer = Game.config.startingPlayer;
