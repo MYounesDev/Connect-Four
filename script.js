f14d8fc41b3282d743a0af1b4f5f53312be8c2bd
MYunus Aboushewil <mohammadyounes686@gmail.com>
2025-02-24 21:52:48 +0300

    merged js files

--- script.js
+++ script.js
@@ -1,30 +1,49 @@
-let myDocument = document.documentElement;
-let btn = document.getElementById("btn");
-
-btn.addEventListener("click", () => {
-  if (btn.textContent == "Go Fullscreen") {
-    if (myDocument.requestFullscreen) {
-      myDocument.requestFullscreen();
-    } else if (myDocument.msRequestFullscreen) {
-      myDocument.msRequestFullscreen();
-    } else if (myDocument.mozRequestFullscreen) {
-      myDocument.mozRequestFullscreen();
-    } else if (myDocument.webkitRequestFullscreen) {
-      myDocument.webkitRequestFullscreen();
+(function () {
+  var prefixEl = document.querySelector("#prefix");
+  var primaryTextEl = document.querySelector(".primary");
+  var secondaryTextEl = document.querySelector(".secondary");
+  var currentPlayerNameEl = document.querySelector("#current-player");
+  var otherPlayerNameEl = document.querySelector("#other-player");
+  var playAgainEl = document.querySelector("#play-again");
+  var playAgainBtnEl = document.querySelector("#play-again-btn");
+  var gameBoardEl = document.querySelector("#board");
+  playAgainBtnEl.addEventListener("click", () => location.reload());
+  gameBoardEl.addEventListener("click", placeGamePiece);
+  currentPlayerNameEl.addEventListener("keydown", Game.do.handleNameChange);
+  otherPlayerNameEl.addEventListener("keydown", Game.do.handleNameChange);
+  function placeGamePiece(e) {
+    if (e.target.tagName !== "BUTTON") return;
+    var targetCell = e.target.parentElement;
+    var targetRow = targetCell.parentElement;
+    var targetRowCells = [...targetRow.children];
+    var gameBoardRowsEls = [...document.querySelectorAll("#board tr")];
+    var y_pos = gameBoardRowsEls.indexOf(targetRow);
+    var x_pos = targetRowCells.indexOf(targetCell);
+    y_pos = Game.do.dropToBottom(x_pos, y_pos);
+    if (Game.check.isPositionTaken(x_pos, y_pos)) {
+      alert(Game.config.takenMsg);
+      return;
     }
-
-    btn.textContent = "Exit Fullscreen";
-  } else {
-    if (document.exitFullscreen) {
-      document.exitFullscreen();
-    } else if (document.msexitFullscreen) {
-      document.msexitFullscreen();
-    } else if (document.mozexitFullscreen) {
-      document.mozexitFullscreen();
-    } else if (document.webkitexitFullscreen) {
-      document.webkitexitFullscreen();
+    Game.do.addDiscToBoard(x_pos, y_pos);
+    Game.do.printBoard();
+    if (
+      Game.check.isVerticalWin() ||
+      Game.check.isHorizontalWin() ||
+      Game.check.isDiagonalWin()
+    ) {
+      gameBoardEl.removeEventListener("click", placeGamePiece);
+      prefixEl.textContent = Game.config.winMsg;
+      currentPlayerNameEl.contentEditable = false;
+      secondaryTextEl.remove();
+      playAgainEl.classList.add("show");
+      return;
+    } else if (Game.check.isGameADraw()) {
+      gameBoardEl.removeEventListener("click", placeGamePiece);
+      primaryTextEl.textContent = Game.config.drawMsg;
+      secondaryTextEl.remove();
+      playAgainEl.classList.add("show");
+      return;
     }
-
-    btn.textContent = "Go Fullscreen";
+    Game.do.changePlayer();
   }
-});
+})();
